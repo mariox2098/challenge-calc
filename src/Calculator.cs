b59f756eae26662f9ba1aad032fdd20d850aa4a4
Chima <chimanwanna@gmail.com>
2024-06-23 14:20:23 -0400

    Requirement 1: Support a maximum of 2 numbers using a comma delimiter. Throw an exception when more than 2 numbers are provided

---
+++ src/Calculator.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Calculator
+{
+    public Calculator() { }
+    public int Calculate(string inputString) {
+        var numbers = ParseStringToNumbers(inputString);
+        var output = RunOperation(numbers);
+        return output;
+    }
+
+    private List<int> ParseStringToNumbers(string inputString)
+    {
+        var parsedInts = new List<int>();   
+        var result = inputString.Split(',');
+        var index = 0;
+
+        foreach (var item in result)
+        {
+            if(index >= 2)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+            else if(int.TryParse(item, out var parsedInt))
+            {
+                parsedInts.Add(parsedInt);
+            }
+            else
+            {
+                parsedInts.Add(0);
+            }
+
+            index++;
+        }
+
+        return parsedInts;
+    }
+
+    private int RunOperation(List<int> inputNumbers)
+    {
+        return inputNumbers.Sum();
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Challenge Calculator ---\n");
+var calculator = new Calculator();
+var input = "5,tytyt";
+
+var result = calculator.Calculate(input);
+
+Console.WriteLine("Input: " + input);
+Console.WriteLine("Output: " + result);
